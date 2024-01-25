06b31b965b1d7a126de25a6ac10d9fb63d9e92f3
bonill4 <williamsbonill11@gmail.com>
2024-01-25 08:30:31 -0600

    Aplicacion de las interfaces

--- Program.cs
+++ Program.cs
@@ -17,11 +17,17 @@ namespace ProyectoHerencia
 
             Gorila miCopito = new Gorila("Copito");
 
-            miJuan.getNombre();
-            miBabieca.getNombre();
-            miCopito.getNombre();
+            //miJuan.getNombre();
+            //miBabieca.getNombre();
+            //miCopito.getNombre();
 
-            Sustitucion();
+            //Sustitucion();
+
+            Ballena miWally = new Ballena("Wally");
+
+            miWally.nadar();
+
+            Console.WriteLine($"Numero de patas de Babieta {miBabieca.NumeroPatas()}");
         }
 
         static public void Sustitucion()
@@ -67,6 +73,22 @@ namespace ProyectoHerencia
         }
     }
 
+    /*INTERFACES:
+     Es como un contrato que una clase firma. Este contrato especifica qué métodos y propiedades debe proporcionar la clase que lo firma. */
+
+    //Todos los nombres de las interfaces deben de iniciar con una "I"
+
+    //Las interfaces pueden poseer muchos metodos
+    interface IMamiferosTerrestres
+    {
+        //Los metodos de una interfaz no se desarrollan 
+
+        //No se abren llaves, no hay codigo dentro de ellas. Simplemente de declaran
+
+        //No poseen modificadores de acceso
+        int NumeroPatas();
+    }
+
     class Mamiferos
     {
         public Mamiferos(String nombre)
@@ -89,6 +111,7 @@ namespace ProyectoHerencia
         
         //Con "virtual" estamos indicando que todas las sub clases de mamiferos
         //deberian de tener un metodo "pensar()",
+
         //En otras palabras que las clases hijas podran tener el mismo metodo 
         //y los cambios que resiva sera una sobreescritura para esta
         public virtual void pensar()
@@ -104,7 +127,10 @@ namespace ProyectoHerencia
     }
 
     //Sintaxis de como hereda de la clase "Mamifero" a "Caballo"
-    class Caballo : Mamiferos
+
+    //Sintaxis de como agregar una interfaz y el orden es: 1° La clase y 2° La interfas o interfaces 
+    //separado por una ","
+    class Caballo : Mamiferos, IMamiferosTerrestres 
     {
         //USO DE LA INSTRUCCION :base()
         /*Si el constructor de la clase padre tiene metodo por defecto podemos presindir 
@@ -119,6 +145,14 @@ namespace ProyectoHerencia
         public void galopar()
         {
             Console.WriteLine("Soy capaz de galopar");
+        }
+        //Al desarollar interfaces dentro de un clase:
+        //1 - El nombre del metodo debe de coincidir con el que se definio en la interfaz
+        //2 - El tipo de dato a devolver debe de coincidir con el que se definio en la interfaz
+        //3 - El numero de parametros debe de coincidir con el numero y tipo con los que se definieron en la interfaz
+        public int NumeroPatas()
+        {
+            return 4;
         }
     }
     class Humano : Mamiferos
@@ -139,7 +173,7 @@ namespace ProyectoHerencia
             Console.WriteLine("Soy capaz de pensar");
         }
     }
-    class Gorila : Mamiferos
+    class Gorila : Mamiferos, IMamiferosTerrestres
     {
         public Gorila (String nombreGorila) : base(nombreGorila)
         {
@@ -152,6 +186,24 @@ namespace ProyectoHerencia
         public void trepar()
         {
             Console.WriteLine("Soy capaz de trepar");
+        }
+
+        public int NumeroPatas()
+        {
+            return 2;
+        }
+    }
+
+    class Ballena : Mamiferos
+    {
+        public Ballena(String nombreBallena):base(nombreBallena)
+        {
+
+        }
+
+        public void nadar()
+        {
+            Console.WriteLine("Soy capaz de nadar");
         }
     }
 }
